8d3e1988fbabefa931567533a64e40377311e4c5
Clement Petja <tcpetja@gmail.com>
2024-11-08 07:43:19 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,11 @@
+const laptopImage = document.getElementById('laptop');
+const videoOverlay = document.getElementById('video-Overlay');
 
+function resizeVideo(){
+const laptopWidth = laptopImage.clientWidth;
+const laptopHeight = laptopimage.clientHeight;
+
+const laptopAspectRatio = laptopWidth/laptopHeight;
+videoOverlay.width = laptopWidth;
+vidioOverlay.height = laptopWidth/laptopAspectRatio;
+}
